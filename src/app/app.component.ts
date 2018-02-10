cff02336bf4ad1ac9de6171604a2560f747397a3
Prabhakar Reddy <prabhakarreddy1234@gmail.com>
2018-02-10 19:52:04 +0000

    missing files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  signIn;
+  router;
+
+  constructor(public oktaAuth: OktaAuthService, router: Router) {
+    this.signIn = oktaAuth;
+    this.router = router;
+  }
+
+  async logout() {
+    // Terminates the session with Okta and removes current tokens.
+    await this.signIn.logout();
+    this.router.navigateByUrl('/');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
+import {
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OktaAuthGuard
+} from '@okta/okta-angular';
 
 import { AppComponent } from './app.component';
+import { ProtectedComponent } from './protected.component';
+import { LoginComponent } from './login.component';
 
+const config = {
+  issuer: 'https://dev-782937.oktapreview.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oadq5bn0qfRlZ9n30h7'
+}
 
+export function onAuthRequired({ oktaAuth, router }) {
+  // Redirect the user to your custom login page
+  router.navigate(['/login']);
+}
+
+const appRoutes: Routes = [
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ OktaAuthGuard ],
+    data: {
+      onAuthRequired
+    }
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),    
+    OktaAuthModule.initAuth(config)
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
